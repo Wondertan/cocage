96806660462bccf3c79a402884c26c3cd852dfc7
Callum Waters <cmwaters19@gmail.com>
2023-10-07 00:25:50 +0200

    add support for the module genesis

--- modules/da/module.go
+++ modules/da/module.go
@@ -1,18 +1,24 @@
 package da
 
 import (
+	"encoding/json"
+	"errors"
+
 	"cosmossdk.io/core/appmodule"
 	"cosmossdk.io/core/store"
 	"cosmossdk.io/depinject"
-	modulev1 "github.com/Wondertan/da/modules/da/v1"
+	sdkerrors "cosmossdk.io/errors"
+	v1 "github.com/Wondertan/da/modules/da/v1"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
+	sdkclient "github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	staking "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	gwruntime "github.com/grpc-ecosystem/grpc-gateway/runtime"
 	celestia "github.com/rollkit/celestia-openrpc"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // ConsensusVersion defines the current da module consensus version.
@@ -20,7 +26,7 @@ const ConsensusVersion = 1
 
 var (
 	_ module.AppModuleBasic = AppModule{}
-	// _ module.HasGenesis     = AppModule{}
+	_ module.HasGenesis     = AppModule{}
 
 	_ appmodule.AppModule = AppModule{}
 )
@@ -44,6 +50,28 @@ func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *g
 func (AppModuleBasic) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 }
 
+// DefaultGenesis returns default genesis state as raw bytes for the da
+// module.
+func (ab AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+	return cdc.MustMarshalJSON(&v1.GenesisState{
+		LatestCounterpartyHeight: 1,
+	})
+}
+
+// ValidateGenesis performs genesis state validation for the da module.
+func (ab AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config sdkclient.TxEncodingConfig, bz json.RawMessage) error {
+	var data v1.GenesisState
+	if err := cdc.UnmarshalJSON(bz, &data); err != nil {
+		return sdkerrors.Wrapf(err, "failed to unmarshal %s genesis state",ModuleName)
+	}
+
+	if data.LatestCounterpartyHeight == 0 {
+		errors.New("latest counterparty height is zero")
+	}
+
+	return nil
+}
+
 // AppModule implements an application module for the da module.
 type AppModule struct {
 	AppModuleBasic
@@ -63,9 +91,32 @@ func (am AppModule) IsOnePerModuleType() {}
 // IsAppModule implements the appmodule.AppModule interface.
 func (am AppModule) IsAppModule() {}
 
+// InitGenesis performs genesis initialization for the da module. It returns
+// no validator updates.
+func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, bz json.RawMessage) {
+	var gs v1.GenesisState
+	cdc.MustUnmarshalJSON(bz, &gs)
+	// TODO: set the initial height
+}
+
+// ExportGenesis returns the exported genesis state as raw bytes for the da
+// module.
+func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
+	height, err := am.keeper.LatestDataCommitmentHeight(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	gs := &v1.GenesisState{
+		LatestCounterpartyHeight: height,
+	}
+
+	return cdc.MustMarshalJSON(gs)
+}
+
 func init() {
 	appmodule.Register(
-		&modulev1.Module{},
+		&v1.Module{},
 		appmodule.Provide(ProvideModule),
 	)
 }
