24dddb0b9e3ae980f869bb27a67a622d8ced2894
Callum Waters <cmwaters19@gmail.com>
2023-10-07 00:15:55 +0200

    add some comments

--- process_proposal.go
+++ process_proposal.go
@@ -10,20 +10,31 @@ import (
 	celestia "github.com/rollkit/celestia-openrpc"
 )
 
+// ProcessProposalHandler is required for using the DA module. It loops to
+// find the first MsgAttestDataCommitment in the block. It then verifies the
+// transaction by taking the height for the last committed data root and
+// checking that each height listed in the message has been sampled and is
+// "available" to the node. Futhermore it checks that the data root matches
+// it's local data root. If this passes, it approves the proposal
 func ProcessProposalHandler(dec sdk.TxDecoder, da da.Keeper, client celestia.Client) sdk.ProcessProposalHandler {
 	return func(ctx sdk.Context, req *abci.RequestProcessProposal) (*abci.ResponseProcessProposal, error) {
-		latestHeight, err := da.LatestDataCommitmentHeight(ctx)
-		if err != nil {
-			return nil, err
-		}
-
 		msg := findDataCommitmentMsg(dec, req.Txs)
 		if msg == nil {
 			return accept(), nil
 		}
 
+		latestHeight, err := da.LatestDataCommitmentHeight(ctx)
+		if err != nil {
+			return nil, err
+		}
+
 		for idx, dataCommitment := range msg.DataCommitments {
 			height := uint64(idx) + latestHeight + 1
+			// TODO: these should theoretically be all the heights
+			// that the node has already sampled as it would have
+			// already indicated the sampled heights in the vote extensions.
+			// What would be more reliable is to cache the sampled header's
+			// data roots so we don't have to make a second remote call
 			header, err := client.Header.GetByHeight(ctx, height)
 			if err != nil {
 				return reject(), nil
